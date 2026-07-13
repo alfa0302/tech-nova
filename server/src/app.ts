29c9c8b2e79d7e7765ad1c2a2e150bf9023ff1e9
alfa0302 <alfiyanijin@gmail.com>
2026-07-13 20:32:22 +0400

    clerk webhook set up

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,23 @@
 import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { clerkMiddleware } from "@clerk/express";
+import { clerkWebhookHandler } from "./webhooks/clerk";
+import { getEnv } from "./lib/env";
+
+const env = getEnv();
 const app = express();
 
-app.listen(3000, () => {
-  console.log("App listening to port 3000");
+// defined before json parsing as raw data is needed
+const rawJSON = express.raw({ type: "application/json", limit: "1mb" });
+app.post("/webhook/clerk", rawJSON, (req, res) => {
+  void clerkWebhookHandler(req, res);
+});
+
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
+
+app.listen(env.PORT, () => {
+  console.log("App listening on port ", env.PORT);
 });
