c7ee5e7bb8f6b03f853b8984b827fb1f7eb34f39
alfa0302 <alfiyanijin@gmail.com>
2026-08-08 09:29:29 +0400

    sentry integration with user and error handling

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { NextFunction, Response, Request } from "express";
 import cors from "cors";
 import "dotenv/config";
 import { clerkMiddleware } from "@clerk/express";
@@ -7,10 +7,12 @@ import { polarWebhookHandler } from "./webhooks/polar";
 import { getEnv } from "./lib/env";
 import path from "path";
 import fs from "fs";
+import * as Sentry from "@sentry/node";
 // routers
 import productRouter from "./routes/productRouter";
 import userRouter from "./routes/userRouter";
 import checkoutRouter from "./routes/checkoutRouter";
+import { sentryClerkUserMiddleware } from "./middleware/sentryClerkUser";
 
 const env = getEnv();
 const app = express();
@@ -27,6 +29,7 @@ app.post("/webhook/polar", rawJSON, (req, res) => {
 app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
+app.use(sentryClerkUserMiddleware);
 
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
@@ -47,6 +50,17 @@ if (fs.existsSync(publicDir)) {
     res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
   });
 }
+
+// attach sentry to res object
+Sentry.setupExpressErrorHandler(app);
+
+app.use((_err: unknown, _req: Request, res: Response, _next: NextFunction) => {
+  const sentryId = res as express.Response & { sentry?: string };
+  res.status(500).json({
+    error: "Internal Server Error",
+    ...(sentryId !== undefined && { sentryId }),
+  });
+});
 
 app.listen(process.env.PORT || env.PORT, () => {
   console.log("App listening on port", process.env.PORT || env.PORT);
